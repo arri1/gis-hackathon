8dc9a6977695ed36c67273a0e1969f07be633c26
arri <ewws@mail.ru>
2020-03-15 15:26:03 +0900

    added fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-const __html = require('./gistDist/index.html');
-const template = { __html: __html };
+
 function App() {
   return (
     <div>
-      <div dangerouslySetInnerHTML={template} />
     </div>
   );
 }
